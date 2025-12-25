ef4dd660b9e98ee3ef8d36f38d43329fc9f5b828
vaishali0312 <shenisettivaishali@gmail.com>
2025-12-25 11:42:34 +0530

    Set up routing with React Router in App.js

---
+++ App.js
@@ -0,0 +1,35 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Todos from "./pages/Todos";
+import TodoDetails from "./pages/TodoDetails";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+
+      <Route
+        path="/todos"
+        element={
+          <ProtectedRoute>
+            <Todos />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/todos/:todoId"
+        element={
+          <ProtectedRoute>
+            <TodoDetails />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
+  );
+}
+
+export default App;
